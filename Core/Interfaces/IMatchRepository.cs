9838d146dda1c670f1e84802d59d05402bd7f5fa
TailBuddys <tailBuddys@gmail.com>
2025-01-31 17:55:25 +0200

    fixes

--- Core/Interfaces/IMatchRepository.cs
+++ Core/Interfaces/IMatchRepository.cs
@@ -11,8 +11,8 @@ namespace TailBuddys.Core.Interfaces
         public Task<List<Match>> GetAllMatchesFromDog(string dogId);
         // get all dog matches from other dogs
         public Task<List<Match>> GetAllMatchesToDog(string dogId);
-        public Task<Match?> GetMatchById(string matchId);
-        public Task<Match?> UpdateMatch(string matchId, Match match);
-        public Task<Match?> DeleteMatch(string matchId);
+        public Task<Match?> GetMatchById(int matchId);
+        public Task<Match?> UpdateMatch(int matchId, Match match);
+        public Task<Match?> DeleteMatch(int matchId);
     }
 }
