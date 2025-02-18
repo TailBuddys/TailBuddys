52586f0ff8b1eddc26093bc611d7f1a305878c1e
MIGELZO <154762117+MIGELZO@users.noreply.github.com>
2025-02-18 20:52:50 +0200

    UserService0.1

--- Application/Services/UserService.cs
+++ Application/Services/UserService.cs
@@ -1,6 +1,42 @@
-﻿namespace TailBuddys.Application.Services
+﻿using TailBuddys.Application.Interfaces;
+using TailBuddys.Core.Interfaces;
+using TailBuddys.Core.Models;
+
+namespace TailBuddys.Application.Services
 {
-    public class UserService
+    public class UserService : IUserService
     {
+        private readonly IUserRepository _userReposetory;
+
+        public UserService(IUserRepository userReposetory)
+        {
+            _userReposetory = userReposetory;
+        }
+
+        public async Task<User?> Register(User user)
+        {
+            if (user == null) return null;
+            return await _userReposetory.CreateUser(user);
+        }
+
+        public async Task<string?> Login()
+        {
+            try
+            {
+                User? u = await _userReposetory.GetUserByEmail("");
+                if (u != null)
+                {
+                    return "";
+                }
+                return null;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
+        }
+
+
     }
 }
