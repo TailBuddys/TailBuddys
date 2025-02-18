55bb42715d1d7b7ca0512d4e7e6c191ea679c5b1
TailBuddys <tailBuddys@gmail.com>
2025-02-18 20:16:24 +0200

    hi

--- Infrastructure/Services/ChatRepository.cs
+++ Infrastructure/Services/ChatRepository.cs
@@ -13,35 +13,36 @@ namespace TailBuddys.Infrastructure.Services
         }
         public async Task<Chat?> CreateChat(string fromDogId, string toDogId)
         {
-
+            return new Chat();
         }
         public async Task<List<Chat?>> GetAllDogChats(string dogId)
         {
+            return new List<Chat>();
 
         }
         public async Task<Chat?> GetChatById(string chatId)
         {
-
+            return new Chat();
         }
         public async Task<Chat?> UpdateChat(string chatId, Chat chat)
         {
-
+            return new Chat();
         }
         public async Task<Chat?> DeleteChat(string chatId)
         {
-
+            return new Chat();
         }
         public async Task<Chat?> AddMessageToChat(string chatId, Message message)
         {
-
+            return new Chat();
         }
         public async Task<List<Message>> GetMessagesByChatId(string chatId)
         {
-
+            return new List<Message>();
         }
         public async Task<Message?> MarkMessageAsRead(string messageId)
         {
-
+            return new Message();
         }
     }
 }
