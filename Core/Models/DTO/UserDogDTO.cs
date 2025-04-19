ed4805b8d94ea28b1f48fb69b36961960a5309d4
TailBuddys <tailBuddys@gmail.com>
2025-04-19 03:25:49 +0300

    fix

--- Core/Models/DTO/UserDogDTO.cs
+++ Core/Models/DTO/UserDogDTO.cs
@@ -1,8 +1,11 @@
-﻿namespace TailBuddys.Core.Models.DTO
+﻿using System.Text.Json.Serialization;
+
+namespace TailBuddys.Core.Models.DTO
 {
     public class UserDogDTO
     {
         public int Id { get; set; }
+        [JsonIgnore]
         public bool? IsBot { get; set; }
         public string? Name { get; set; }
         public string? ImageUrl { get; set; }
