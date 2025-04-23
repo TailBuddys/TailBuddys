85f7d1dc2c62c3238043387ed42b309601e96073
TailBuddys <tailBuddys@gmail.com>
2025-04-23 20:59:16 +0300

    hide keys and fixes in program

--- Program.cs
+++ Program.cs
@@ -40,14 +40,16 @@ namespace TailBuddys
 
                 // Add services to the container
 
-                string? ConnectionString = Environment.GetEnvironmentVariable("TailBuddysDBString", EnvironmentVariableTarget.User);
+                var dbConnectionString = Environment.GetEnvironmentVariable("TailBuddysDBString")
+                        ?? builder.Configuration.GetConnectionString("DefaultConnection");
 
-                if (string.IsNullOrEmpty(ConnectionString))
+
+                if (string.IsNullOrEmpty(dbConnectionString))
                 {
-                    throw new Exception("connection string is required");
+                    throw new Exception("Missing DB connection string.");
                 }
                 builder.Services.AddControllers();
-                builder.Services.AddDbContext<TailBuddysContext>(options => options.UseSqlServer(ConnectionString));
+                builder.Services.AddDbContext<TailBuddysContext>(options => options.UseSqlServer(dbConnectionString));
 
                 // Register ActiveDogs as a singleton
                 builder.Services.AddSingleton<IDogConnectionTracker, DogConnectionTracker>();
@@ -93,10 +95,14 @@ namespace TailBuddys
                             ValidateAudience = true,
                             ValidateLifetime = true,
 
-                            ValidIssuer = "TailBuddysServer",
-                            ValidAudience = "TailBuddysApp",
-                            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
-                                "31cb3b1a-f4f3-466e-9099-d4f49a0dd4b8"))// Consider securing this via secret storage
+                            //ValidIssuer = "TailBuddysServer",
+                            //ValidAudience = "TailBuddysApp",
+                            //IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                            //    "31cb3b1a-f4f3-466e-9099-d4f49a0dd4b8"))// Consider securing this via secret storage
+                            ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+                            ValidAudience = builder.Configuration["JwtSettings:Audience"],
+                            IssuerSigningKey = new SymmetricSecurityKey(
+                                    Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:Secret"] ?? throw new Exception("Missing JWT secret")))
                         };
 
                         // Allow SignalR access token from query string
@@ -150,9 +156,6 @@ namespace TailBuddys
                     app.UseSwagger();
                     app.UseSwaggerUI();
                 }
-
-                app.UseCors("myCorsPolicy");
-
 
                 app.UseHttpsRedirection();
                 app.UseCors();
